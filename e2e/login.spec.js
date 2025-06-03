ba5f018044a53ef00dffd46845c7d01e003fe107
Lívia Esteves <liviaesteves359@gmail.com>
2025-06-02 22:19:11 -0300

    add login test with valid credentials

--- e2e/login.spec.js
+++ e2e/login.spec.js
@@ -5,6 +5,11 @@ test.describe("Login Page Test", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("https://www.saucedemo.com/");
     await expect(page.locator("text=Swag Labs")).toBeVisible();
-    await login()
   });
+
+  test("Login with valid credentials", async ({ page }) => {
+    await login("standard_user", "secret_sauce");
+    await expect(page.locator("text=Products")).toBeVisible();
+  });
+  
 });
