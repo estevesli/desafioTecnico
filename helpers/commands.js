270b45596b5c4cc75189ce994cfd2aa7341cbdcd
Lívia Esteves <liviaesteves359@gmail.com>
2025-06-03 01:26:23 -0300

    add fill checkout information function

--- helpers/commands.js
+++ helpers/commands.js
@@ -5,3 +5,9 @@ async function login(page,email, password){
     await page.locator('input[data-test="login-button"]').click();
 
 }
+async function checkoutInformation(page, firstName, lastName, postalCode){
+    await page.locator('input[data-test="firstName"]').fill(firstName);
+    await page.locator('input[data-test="lastName"]').fill(lastName);
+    await page.locator('input[data-test="postalCode"]').fill(postalCode);
+    await page.locator('input[data-test="continue"]').click();
+}
