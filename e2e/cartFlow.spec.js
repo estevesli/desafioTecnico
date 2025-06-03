af9fa5b126781692fd85ed919f0ebb3a011d9873
Lívia Esteves <liviaesteves359@gmail.com>
2025-06-03 01:44:32 -0300

    add back to items page test

--- e2e/cartFlow.spec.js
+++ e2e/cartFlow.spec.js
@@ -7,7 +7,14 @@ test.describe("Login Page Test", () => {
     await login(page, "standard_user", "secret_sauce");
     await expect(page.locator("text=Products")).toBeVisible();
   });
-
+  
+  test('Goes back to product list', async ({ page }) => {
+    await page.locator('[data-test="inventory-item-name"]', { hasText: 'Sauce Labs Bike Lights' }).click();
+    const backButton = page.locator('[data-test="back-to-products"]');
+    await backButton.waitFor({ state: 'visible', timeout: 5000 });
+    await page.locator('input[data-test="back-to-products"]').click();
+     await expect(page.locator("text=Products")).toBeVisible();
+  });
   test('Add to cart through product page', async ({ page }) => {
     await page.locator('[data-test="inventory-item-name"]', { hasText: 'Sauce Labs Backpack' }).click();
     const backButton = page.locator('[data-test="back-to-products"]');
