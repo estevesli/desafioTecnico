da3ac3991ec06798981cf24a0bfd77cb27ab04fd
Lívia Esteves <liviaesteves359@gmail.com>
2025-06-03 00:18:19 -0300

    fix login function

--- e2e/login.spec.js
+++ e2e/login.spec.js
@@ -8,23 +8,23 @@ test.describe("Login Page Test", () => {
   });
 
   test("Login with valid credentials", async ({ page }) => {
-    await login("standard_user", "secret_sauce");
+    await login(page, "standard_user", "secret_sauce");
     await expect(page.locator("text=Products")).toBeVisible();
   });
   test("Login with invalid credentials", async ({ page }) => {
-      await login('standard_user', 'srcretsauce');
+      await login(page, 'invalid_user', 'invalid_password');
       await expect(page.locator('[data-test="error"]')).toBeVisible();
   })
   test('Login with locked out user', async ({ page }) => {
-    await login('locked_out_user', 'secret_sauce');
+    await login(page, 'locked_out_user', 'secret_sauce');
     await expect(page.locator("text=Epic sadface: Sorry, this user has been locked out.")).toBeVisible();
   })
   test('Login with no credentials', async ({ page }) => {
-    await login('', '');
+    await login(page, '', '');
     await expect(page.locator('[data-test="error"]')).toBeVisible();
   })
   test(('Logout'), async ({ page }) => {
-    await login('standard_user', 'secret_sauce');
+    await login(page, 'standard_user', 'secret_sauce');
     await page.locator('.bm-burger-button').click();
     await page.locator('[data-test="logout-sidebar-link"]').click();
     await expect(page.locator('input[data-test="login-button"]')).toBeVisible();
--- helpers/commands.js
+++ helpers/commands.js
@@ -1,9 +1,7 @@
-async function login(email, password){
+export default login;
+async function login(page,email, password){
     await page.locator('input[data-test="username"]').fill(email);
     await page.locator('input[data-test="password"]').fill(password);
     await page.locator('input[data-test="login-button"]').click();
 
 }
-
-
-export default {login}
